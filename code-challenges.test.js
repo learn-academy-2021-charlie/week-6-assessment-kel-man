9f690ec7dfb96ba3fe0d0e0a86569f929d4cd4ef
kel-man <kyyafuso@email.com>
2021-08-13 14:05:06 -0700

    code challenges complete

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -23,13 +23,31 @@ var people = [
   { name: "zaphod beeblebrox", occupation: "president of the galaxy" },
   { name: "arthur dent", occupation: "radio employee" }
 ]
+
+describe('stringFormatter', () => {
+  it('capitalizes the names and interpolates properly', () => {
+    expect(stringFormatter(people)).toEqual(["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is a president of the galaxy.", "Arthur Dent is a radio employee."])
+  })
+})
 // Expected output: ["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is a president of the galaxy.", "Arthur Dent is a radio employee."]
 
-
+/*
+ * Loop through the array with .map(), using an anonymous function with arguments of destructured key value pairs, i.e. array.map(({name, occupation}) => {return sentence})
+ * return the output of that map
+ * inside map capitalize each word in name with name.split(' ') and a loop
+ * interpolate using both variables
+*/
 
 // b) Create the function that makes the test pass.
 
+const stringFormatter = people => {
+  return people.map(({name, occupation}) => {
+    let capital = name.split(' ').map(str => str[0].toUpperCase() + str.substr(1)).join(' ') // adds the first letter capitalized to the substring of the word starting at the 2nd letter
+    return `${capital} is a ${occupation}.`
+  })
+}
 
+// Code Golf first try!
 
 // --------------------2) Create a function that takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3.
 
@@ -40,10 +58,31 @@ var hodgepodge1 = [23, "Heyyyy!", 45, -10, 0, "Yo", false]
 var hodgepodge2 = [5, "Hola", 43, -34, "greetings", true]
 // Expected output: [ 2, 1, -1 ]
 
+describe('modThree', () => {
+  it('returns an array of only the remainders of number values divided by three', () => {
+    expect(modThree(hodgepodge1)).toEqual([2, 0, -1, 0])
+    expect(modThree(hodgepodge2)).toEqual([2, 1, -1])
+  })
+})
 
+/*
+ * I could filter the array for numbers first and then map out an output of the numbers % 3 but that involves using two loops, resulting in O(2N)
+ * while this O notation is basically the same as O(N) it's still technically less efficient than just doing one loop
+ * So I'll use a for loop and create a storage array outside the loop to push my values to
+ * Check if each value has typeof 'number'
+ * if it does, storage.push(number%3)
+ * return the storage
+*/
 
 // b) Create the function that makes the test pass.
 
+const modThree = data => {
+  let output = []
+  for(let i=0; i<data.length; i++){
+    typeof data[i] === 'number' && output.push(data[i]%3)
+  }
+  return output
+}
 
 
 // --------------------3) Create a function that takes in an array of numbers and returns the sum of all the numbers cubed.
@@ -55,6 +94,23 @@ var cubeAndSum1 = [2, 3, 4]
 var cubeAndSum2 = [0, 5, 10]
 // Expected output: 1125
 
+describe('cubeSum', () => {
+  it('cubes each number then sums the indices', () => {
+    expect(cubeSum(cubeAndSum1)).toEqual(99)
+    expect(cubeSum(cubeAndSum2)).toEqual(1125)
+  })
+})
 
+/*
+ * create an output variable of type number initialized at 0
+ * forEach index in the array, add the value cubed to the output variable
+ * return the output variable
+*/
 
 // b) Create the function that makes the test pass.
+
+const cubeSum = nums => {
+  let output = 0
+  nums.forEach(num => output += num**3)
+  return output
+}
